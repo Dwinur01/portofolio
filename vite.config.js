4357fe406c347cefcf128c35a7a877d4a36213b0
Dwinur01 <dwinurhidayat20@gmail.com>
2025-10-10 18:39:43 +0700

    style

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Pastikan baris ini ada
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Pastikan baris ini ada
-  ],
+  plugins: [react()],
 })
